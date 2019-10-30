932e290b332031dfd51bd960535cc9f8c31e2191
andrea-w-wang <ayw255@nyu.edu>
2019-10-30 15:43:47 -0400

    first commit - scrapy

---
+++ scrapy_implementation/newsSpiders/spiders/news_spider.py
@@ -0,0 +1,27 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from datetime import datetime, timedelta
+
+
+class NewsSpider(CrawlSpider):
+    name = 'news'
+
+    def __init__(self, site_url='', article_url_patterns='', following_url_patterns='', *args, **kwargs):
+        super(NewsSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [site_url]
+        article_url_patterns = article_url_patterns.split('; ')
+        following_url_patterns = following_url_patterns.split('; ')
+        print(article_url_patterns)
+        NewsSpider.rules = [Rule(LinkExtractor(allow=article_url_patterns), callback="parse_articles")]
+        if following_url_patterns:
+            NewsSpider.rules.append(Rule(LinkExtractor(allow=following_url_patterns), follow=True))
+        super(NewsSpider, self)._compile_rules()
+
+    def parse_articles(self, response):
+        parse_time = datetime.utcnow() + timedelta(hours=8)
+        parse_time = str(parse_time.strftime('%Y-%m-%d-%H:%M:%S'))
+        yield {'url': response.url,
+               'title': ' '.join(response.css('h1 *::text').extract()).strip(),
+               # 'html': response.body,
+               'fetched_at': parse_time}
+
