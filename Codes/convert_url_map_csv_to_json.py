3cc3e4ca8f8d76cb5e6fb58ee6aa8070265af494
andrea-w-wang <ayw255@nyu.edu>
2019-11-06 17:51:35 -0500

    create article and article_snapshot

--- Codes/convert_url_map_csv_to_json.py
+++ Codes/convert_url_map_csv_to_json.py
@@ -1,11 +1,12 @@
-import json
 import pandas as pd
-import sys
+import os
 
-file_path = 'url_map.csv'
+root_dir = os.getcwd().split('/NewsScraping/')[0]+'/NewsScraping'
+data_dir = root_dir+'/Data'
+file_path = data_dir+'/url_map.csv'
 url_map_df = pd.read_csv(file_path)
 url_map_df = url_map_df.set_index('id')
-url_map_df.to_json('url_map.json', orient='index')
+url_map_df.to_json(data_dir+'/url_map.json', orient='index')
 
 # read
-# json.load(open('url_map.json', 'r'))
+# json.load(open(data_dir+'url_map.json', 'r'))
