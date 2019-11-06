d022a4cf1d50c869329a38d2d950b1fa88962bff
andrea-w-wang <ayw255@nyu.edu>
2019-11-06 12:36:15 -0500

    json

--- scrapy_implementation/convert_url_map_csv_to_json.py
+++ scrapy_implementation/convert_url_map_csv_to_json.py
@@ -2,5 +2,10 @@ import json
 import pandas as pd
 import sys
 
-file_path = sys.argv[0]
-df = pd.read_csv(file_path)
+file_path = 'url_map.csv'
+url_map_df = pd.read_csv(file_path)
+url_map_df = url_map_df.set_index('id')
+url_map_df.to_json('url_map.json', orient='index')
+
+# read
+# json.load(open('url_map.json', 'r'))
--- scrapy_implementation/newsSpiders/settings.py
+++ scrapy_implementation/newsSpiders/settings.py
@@ -67,7 +67,7 @@ ROBOTSTXT_OBEY = False
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'newsSpiders.pipelines.NewsspidersPipeline': 300,
+#    'newsSpiders.pipelines.newsSpidersPipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
