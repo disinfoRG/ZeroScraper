4850bc1eb4e71fb2ffcf5320b84d820434446b6a
andrea-w-wang <ayw255@nyu.edu>
2019-11-06 17:50:59 -0500

    create artical and artical snapshot with filtering existing urls

--- Codes/newsSpiders/settings.py
+++ Codes/newsSpiders/settings.py
@@ -66,9 +66,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'newsSpiders.pipelines.newsSpidersPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'newsSpiders.pipelines.MultiJSONPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
--- Codes/newsSpiders/spiders/news_spider.py
+++ Codes/newsSpiders/spiders/news_spider.py
@@ -1,6 +1,7 @@
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from datetime import datetime, timedelta
+from newsSpiders.items import ArticleItem
 
 
 class NewsSpider(CrawlSpider):
@@ -12,18 +13,22 @@ class NewsSpider(CrawlSpider):
         self.start_urls = [site_url]
         article_url_patterns = article_url_patterns.split('; ')
         following_url_patterns = following_url_patterns.split('; ')
-        print(article_url_patterns)
         NewsSpider.rules = [Rule(LinkExtractor(allow=article_url_patterns), callback="parse_articles")]
         if following_url_patterns:
             NewsSpider.rules.append(Rule(LinkExtractor(allow=following_url_patterns), follow=True))
         super(NewsSpider, self)._compile_rules()
 
     def parse_articles(self, response):
+        article = ArticleItem()
         parse_time = datetime.utcnow() + timedelta(hours=8)
         parse_time = str(parse_time.strftime('%Y-%m-%d-%H:%M:%S'))
-        yield {"from_site": self.site_id,
-               "url": response.url,
-               "title": " ".join(response.css('h1 *::text').extract()).strip(),
-               "raw_body": response.text,
-               "fetched_at": parse_time}
+        article['site_id'] = self.site_id
+        article['url'] = response.url
+        article['raw_body'] = response.text
+        article['found_at'] = parse_time
+        article['fetched_at'] = parse_time
+        article['article_id'] = article['url'] + '/' + article['found_at']
+        if 'redirect_urls' in response.meta.keys():
+            article['redirect_from'] = response.meta['redirect_urls']
 
+        yield article
