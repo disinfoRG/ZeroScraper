7a2b9bd484fd8cd3fb3c6a36b784f0ee3da89508
andrea-w-wang <ayw255@nyu.edu>
2019-11-08 17:47:38 -0500

    add crawler for update article

--- Codes/newsSpiders/pipelines.py
+++ Codes/newsSpiders/pipelines.py
@@ -5,10 +5,9 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-from scrapy.exceptions import DropItem
+
 from scrapy.exporters import JsonLinesItemExporter
 import os
-import jsonlines
 
 
 class MultiJSONPipeline(object):
@@ -17,24 +16,14 @@ class MultiJSONPipeline(object):
         # dispatcher.connect(self.spider_opened, signal=signals.spider_opened)
         # dispatcher.connect(self.spider_closed, signal=signals.spider_closed)
         self.data_dir = os.getcwd().split('/NewsScraping/')[0] + '/NewsScraping/Data'
-        try:
-            self.existing_urls = [obj['url'] for obj in jsonlines.open(f'{self.data_dir}/article.jl')]
-        except FileNotFoundError:
-            self.existing_urls = list()
         self.files = None
         self.exporters = None
 
     def open_spider(self, spider):
-        self.files = {'article': open(f'{self.data_dir}/article.jl', 'ab'),
-                      'article_snapshot': open(f'{self.data_dir}/article_snapshot.jl', 'ab')}
-        self.exporters = {'article': JsonLinesItemExporter(self.files['article'],
-                                                           fields_to_export=['article_id', 'site_id', 'url', 'found_at',
-                                                                             'last_fetched_at', 'next_fetch_at', 'fetch_count',
-                                                                             'redirect_from'],
-                                                           export_empty_fields=False),
-                          'article_snapshot': JsonLinesItemExporter(self.files['article_snapshot'],
-                                                                    fields_to_export=['article_id', 'fetched_at', 'raw_body'],
-                                                                    export_empty_fields=False)}
+        self.files = {'article': open(f'{self.data_dir}/article.jsonl', 'ab'),
+                      'article_snapshot': open(f'{self.data_dir}/article_snapshot.jsonl', 'ab')}
+        self.exporters = {'article': JsonLinesItemExporter(self.files['article'], export_empty_fields=False),
+                          'article_snapshot': JsonLinesItemExporter(self.files['article_snapshot'], export_empty_fields=False)}
         # start exporter
         [e.start_exporting() for e in self.exporters.values()]
 
@@ -44,9 +33,14 @@ class MultiJSONPipeline(object):
         [f.close() for f in self.files.values()]
 
     def process_item(self, item, spider):
-        # todo: more efficient way of checking?
-        if item['url'] in self.existing_urls:
-            raise DropItem("url already existed!")
-        else:
-            self.exporters['article_snapshot'].export_item(item)
-            self.exporters['article'].export_item(item)
+        if spider.name == 'discover_new_articles':
+            for key in item.keys():
+                self.exporters[key].export_item(item[key])
+
+        elif spider.name == 'update_contents':
+            self.exporters['article_snapshot'].export_item(item['article_snapshot'])
+
+            # todo: how to update article.jsonl
+
+
+
--- README.md
+++ README.md
@@ -1,8 +1,23 @@
-## NewsScraping
+# NewsScraping
 Scrape News contents provided in this [target list](https://airtable.com/tbl3DrYs5mXgl0EV9/viw2cuXweY8OxNkX6?blocks=hide).
 
-
+### Running
 ```sh
 $ cd Codes/newsSpiders
-$ python execute_spider.py --site_id <site_id>
 ```
+1. Find new articles for sites listed in [url_map.csv](Data/url_map.csv). The new article metadata will be appended to the jsonlines file [article.jsonl](Data/article.jsonl) and html will be appended to [article_snapshot.jsonl](Data/article_snapshot.jsonl)
+```
+$ python execute_spiders.py --discover --site_id <site_id> 
+```
+2. Revisit news articles in database based on next_fetch_at parameter in [article.jsonl](Data/article.jsonl). 
+The function will save new html to [article_snapshot.jsonl](Data/article_snapshot.jsonl) and (_ideally but not yet implemented_) update the versioning parameters in [article.jsonl](Data/article.jsonl)
+```
+$ python execute_spiders.py --update --site_id <site_id>
+```
+
+### To-dos and Comments
+>1. the update function is not complete yet -- it currently stores article snapshots but does not update the original 
+article list for arguments such as fetch_count and next_fetch_date. Might wait for database connection.
+
+>2. need a way to store update frequency for different online content and find a way to connect with the spider so 
+crawl frequency parameters (such as next_fetch_at and fetch_count) could be updated.
