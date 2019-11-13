7a6769a3d9b6cf85b2a0c6f079b86512fc67158d
Pomin Wu <pomin5@gmail.com>
2019-11-13 08:43:28 +0800

    choir: add black and pre-commit

--- README.md
+++ README.md
@@ -9,6 +9,13 @@ Requires Python 3.6.
 ```sh
 $ pipenv install --dev
 $ pipenv shell
+
+# run db migrations
 $ alembic upgrade head
+
+# run spiders
 $ scrapy crawl "nooho-怒吼文章"
+
+# install pre-commit hooks, and then add your code
+$ pre-commit install
 ```
--- zeroarchive/middlewares.py
+++ zeroarchive/middlewares.py
@@ -53,7 +53,7 @@ class ZeroarchiveSpiderMiddleware(object):
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class ZeroarchiveDownloaderMiddleware(object):
@@ -100,4 +100,4 @@ class ZeroarchiveDownloaderMiddleware(object):
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
