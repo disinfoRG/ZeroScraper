ffdcc45ecf4f43aa18bd3324b9cd122d7f3423ec
Pomin Wu <pomin5@gmail.com>
2019-11-22 10:24:08 +0800

    choir: load db connection from dotenv in alembic

--- migrations/env.py
+++ migrations/env.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -8,6 +13,7 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
