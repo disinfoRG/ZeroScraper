a842e7d8cfcce6560bbf7c15c255d213bbd01bad
Pomin Wu <pomin5@gmail.com>
2019-12-01 19:11:52 +0800

    choir: drop unused files

--- collect_new_urls.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd scrapers
-for python_file in *.py; do python $python_file --collect_new_urls; done
--- update_contents.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd scrapers
-for python_file in *.py; do python $python_file --update_contents; done
