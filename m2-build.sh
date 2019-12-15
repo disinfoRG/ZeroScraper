110cb56a5f865c77e953ab531db6921c52246b59
Pomin Wu <pomin5@gmail.com>
2019-12-15 17:09:01 +0800

    fix: chromedriver download url

--- m2-build.sh
+++ m2-build.sh
@@ -1,6 +1,8 @@
+CHROMEDRIVER_URL=https://chromedriver.storage.googleapis.com/chromedriver_linux64.zip
+
 apt-get install -y libnss3-dev
 if [ ! -x /usr/local/bin/chromedriver ]; then
-  curl -O https://chromedriver.storage.googleapis.co77.0.3865.40/chromedriver_linux64.zip
+  curl -O ${CHROMEDRIVER_URL}
   unzip -o chromedriver_linux64.zip
   mv chromedriver /usr/local/bin
   chmod 700 /usr/local/bin/chromedriver
