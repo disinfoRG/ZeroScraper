2848695c2eff03768fdc16ea20ac6216380239d4
andrea-w-wang <ayw255@nyu.edu>
2019-12-18 18:38:47 -0500

    remove install chrome commands

--- m2-build.sh
+++ m2-build.sh
@@ -1,5 +1,4 @@
 CHROMEDRIVER_URL=https://chromedriver.storage.googleapis.com/79.0.3945.36/chromedriver_linux64.zip
-CHROME_URL=https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 apt-get install -y libnss3-dev
 if [ ! -x /usr/local/bin/chromedriver ]; then
@@ -7,11 +6,6 @@ if [ ! -x /usr/local/bin/chromedriver ]; then
   unzip -o chromedriver_linux64.zip
   mv chromedriver /usr/local/bin
   chmod 700 /usr/local/bin/chromedriver
-fi
-
-if [ ! -x /chrome.deb ]; then
-  curl ${CHROMEDRIVER_URL} -o /chrome.deb
-  dpkg -i /chrome.deb || apt-get install -yf
 fi
 
 pip3 install pipenv && pipenv install --system
