cabb1f819a0674f36f022cc60b5b3aa1975f97dd
andrea-w-wang <ayw255@nyu.edu>
2019-12-18 18:15:10 -0500

    install chrome

--- m2-build.sh
+++ m2-build.sh
@@ -1,14 +1,16 @@
-CHROMEDRIVER_URL=https://chromedriver.storage.googleapis.com/$(curl http://chromedriver.storage.googleapis.com/LATEST_RELEASE)/chromedriver_linux64.zip
+CHROMEDRIVER_URL=https://chromedriver.storage.googleapis.com/79.0.3945.36/chromedriver_linux64.zip
+CHROME_URL=https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 apt-get install -y libnss3-dev
-#if [ ! -x /usr/local/bin/chromedriver ]; then
-#  curl -O ${CHROMEDRIVER_URL}
-#  unzip -o chromedriver_linux64.zip
-#  mv chromedriver /usr/local/bin
-#  chmod 700 /usr/local/bin/chromedriver
-#fi
-curl -O ${CHROMEDRIVER_URL}
-unzip -o chromedriver_linux64.zip
-mv chromedriver /usr/local/bin
-chmod 700 /usr/local/bin/chromedriver
+if [ ! -x /usr/local/bin/chromedriver ]; then
+  curl -O ${CHROMEDRIVER_URL}
+  unzip -o chromedriver_linux64.zip
+  mv chromedriver /usr/local/bin
+  chmod 700 /usr/local/bin/chromedriver
+fi
+if [ ! -x /usr/local/bin/chrome.deb ]; then
+  curl ${CHROMEDRIVER_URL} -o /usr/local/bin/chrome.deb
+  dpkg -i /chrome.deb || apt-get install -yf
+fi
+
 pip3 install pipenv && pipenv install --system
