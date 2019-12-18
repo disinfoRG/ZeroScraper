9be3df484cc9a2570c1df49dd36fcf9493b14279
andrea-w-wang <ayw255@nyu.edu>
2019-12-18 18:07:41 -0500

    reinstall chrome

--- m2-build.sh
+++ m2-build.sh
@@ -1,10 +1,14 @@
 CHROMEDRIVER_URL=https://chromedriver.storage.googleapis.com/$(curl http://chromedriver.storage.googleapis.com/LATEST_RELEASE)/chromedriver_linux64.zip
 
 apt-get install -y libnss3-dev
-if [ ! -x /usr/local/bin/chromedriver ]; then
-  curl -O ${CHROMEDRIVER_URL}
-  unzip -o chromedriver_linux64.zip
-  mv chromedriver /usr/local/bin
-  chmod 700 /usr/local/bin/chromedriver
-fi
+#if [ ! -x /usr/local/bin/chromedriver ]; then
+#  curl -O ${CHROMEDRIVER_URL}
+#  unzip -o chromedriver_linux64.zip
+#  mv chromedriver /usr/local/bin
+#  chmod 700 /usr/local/bin/chromedriver
+#fi
+curl -O ${CHROMEDRIVER_URL}
+unzip -o chromedriver_linux64.zip
+mv chromedriver /usr/local/bin
+chmod 700 /usr/local/bin/chromedriver
 pip3 install pipenv && pipenv install --system
