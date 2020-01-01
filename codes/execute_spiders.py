fc80decc20de13ba9144d387cd3b62ab2dcbba0e
Pomin Wu <pomin5@gmail.com>
2020-01-01 13:14:16 +0800

    refactor: extract newsSpiders.* modules
    - move run_* to newsSpiders.runner.*.run

--- codes/execute_spiders.py
+++ codes/execute_spiders.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import sqlalchemy as db
 from helpers import connect_to_db
+from newsSpiders.runner import discover, update
 
 
 parser = argparse.ArgumentParser()
@@ -24,8 +25,6 @@ parser.add_argument(
 
 # set up
 args = parser.parse_args()
-_, connection, tables = connect_to_db()
-site = tables["Site"]
 defaults = {
     "depth": 0,
     "delay": 1.5,
@@ -34,62 +33,11 @@ defaults = {
 
 
 def run_discover(args, defaults):
-    args.site_id = int(args.site_id)
-    query = db.select([site.columns.url, site.columns.type, site.columns.config]).where(
-        site.columns.site_id == args.site_id
-    )
-    site_info = dict(connection.execute(query).fetchone())
-    site_url = site_info["url"]
-    site_type = site_info["type"]
-    article_pattern = site_info["config"]["article"]
-    following_pattern = site_info["config"].get("following", "")
-    if args.depth is not None:
-        depth = args.depth
-    else:
-        depth = site_info["config"].get("depth", defaults["depth"])
-
-    if args.delay is not None:
-        delay = args.delay
-    else:
-        delay = site_info["config"].get("delay", defaults["delay"])
-    if args.ua is not None:
-        site_ua = args.ua
-    else:
-        site_ua = site_info["config"].get("ua", defaults["ua"])
-    if args.selenium is not None:
-        selenium = args.selenium
-    else:
-        selenium = site_info["config"].get("selenium", False)
-
-    os.system(
-        f"scrapy crawl discover_new_articles \
-                -a site_id='{args.site_id}' \
-                -a site_url='{site_url}' \
-                -a site_type='{site_type}' \
-                -a article_url_patterns='{article_pattern}' \
-                -a following_url_patterns='{following_pattern}' \
-                -a selenium='{selenium}' \
-                -s DEPTH_LIMIT={depth} \
-                -s DOWNLOAD_DELAY={delay} \
-                -s USER_AGENT='{site_ua}'"
-    )
+    return discover.run(args, defaults)
 
 
 def run_update(args, defaults):
-    if args.delay is not None:
-        delay = args.delay
-    else:
-        delay = defaults["delay"]
-    if args.ua is not None:
-        ua = args.ua
-    else:
-        ua = defaults["ua"]
-
-    os.system(
-        f"scrapy crawl update_contents \
-                -s DOWNLOAD_DELAY={delay} \
-                -s USER_AGENT='{ua}'"
-    )
+    return update.run(args, defaults)
 
 
 # execute
