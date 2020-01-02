fa65d7b4bc32f57ce90c8e6d2132eba696c277ec
Pomin Wu <pomin5@gmail.com>
2020-01-02 12:07:28 +0800

    fix: add site_info to airtable update

--- airtable/queries/upsert_site.sql
+++ airtable/queries/upsert_site.sql
@@ -1,7 +1,7 @@
 -- :name upsert_site :insert
 INSERT INTO Site
-(airtable_id, name, type, url, config)
+(airtable_id, name, type, url, config, site_info)
 VALUES
-(:airtable_id, :name, :type, :url, :config)
+(:airtable_id, :name, :type, :url, :config, :site_info)
 ON DUPLICATE KEY UPDATE
-name = :name, type = :type, url = :url, config = :config
+name = :name, type = :type, url = :url, config = :config, site_info = :site_info
