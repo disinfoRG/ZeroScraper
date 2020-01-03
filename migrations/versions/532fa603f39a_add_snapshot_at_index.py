715b858b7fd22d3c3cc7139220fef49e966f8183
Pomin Wu <pomin5@gmail.com>
2020-01-03 18:21:19 +0800

    feat: add snapshot_at index
    - need this index to speed up selecting snapshots to parse

--- migrations/versions/532fa603f39a_add_snapshot_at_index.py
+++ migrations/versions/532fa603f39a_add_snapshot_at_index.py
@@ -1,7 +1,7 @@
 """add snapshot_at index
 
 Revision ID: 532fa603f39a
-Revises: 57f2f8bca25a
+Revises: 2f07cdaab83d
 Create Date: 2020-01-03 18:14:48.681396
 
 """
@@ -11,16 +11,16 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "532fa603f39a"
-down_revision = "57f2f8bca25a"
+down_revision = "2f07cdaab83d"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     op.create_index(
-        "idx_article_snapshot_snapshot_at", "ArticleSnapshot", ["snapshot_at"]
+        "idx_ArticleSnapshot_snapshot_at", "ArticleSnapshot", ["snapshot_at"]
     )
 
 
 def downgrade():
-    op.drop_index("idx_article_snapshot_snapshot_at", "ArticleSnapshot")
+    op.drop_index("idx_ArticleSnapshot_snapshot_at", "ArticleSnapshot")
