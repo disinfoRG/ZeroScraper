625cea5cfb97d7837b2863800b7e08985dc5a740
andrea-w-wang <ayw255@nyu.edu>
2020-01-09 12:28:54 +0800

    follow article

--- newsSpiders/middlewares.py
+++ newsSpiders/middlewares.py
@@ -7,7 +7,7 @@
 
 from scrapy import signals
 from selenium import webdriver
-from scrapy.http import HtmlResponse
+from scrapy.http import Request, HtmlResponse
 import time
 import os
 
@@ -61,7 +61,7 @@ class NewsspidersSpiderMiddleware(object):
         spider.logger.info("Spider opened: %s" % spider.name)
 
 
-class NewsspidersDownloaderMiddleware(object):
+class SeleniumDownloaderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
@@ -130,3 +130,42 @@ class NewsspidersDownloaderMiddleware(object):
         if self.driver:
             print("\nShutting down selenium driver...")
             self.driver.quit()
+
+
+class CookiesDownloaderMiddleware(object):
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the downloader middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_request(self, request, spider):
+        if "ptt.cc" in request.url:
+            request.cookies = {"over18": "1"}
+
+    def process_response(self, request, response, spider):
+        # Called with the response returned from the downloader.
+
+        # Must either;
+        # - return a Response object
+        # - return a Request object
+        # - or raise IgnoreRequest
+        return response
+
+    def process_exception(self, request, exception, spider):
+        # Called when a download handler or a process_request()
+        # (from other downloader middleware) raises an exception.
+
+        # Must either:
+        # - return None: continue processing this exception
+        # - return a Response object: stops process_exception() chain
+        # - return a Request object: stops process_exception() chain
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info("Spider opened: %s" % spider.name)
