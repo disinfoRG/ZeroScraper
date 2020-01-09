80a1cfb46b692f46b79e06b420fb6c986eb2425a
andrea-w-wang <ayw255@nyu.edu>
2020-01-09 12:42:51 +0800

    change default depth to 5

--- newsSpiders/types.py
+++ newsSpiders/types.py
@@ -22,7 +22,7 @@ class SiteConfig(scrapy.Item):
     def default():
         return SiteConfig(
             {
-                "depth": 0,
+                "depth": 5,
                 "delay": 1.5,
                 "ua": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
                 "following": "",
