79b4471bac7c798e0cfddecd75cfa1fbeb4aec5b
Pomin Wu <pomin5@gmail.com>
2020-01-09 17:09:49 +0800

    wip: replace multiprocessing with twisted reactor

--- queries/get_sites_to_crawl.sql
+++ queries/get_sites_to_crawl.sql
@@ -1,3 +1,4 @@
 -- :name get_sites_to_crawl :many
 select site_id, name, last_crawl_at from Site
 where is_active=1 and type in ('official_media', 'content_farm', 'organization_website', 'news_website')
+order by last_crawl_at asc
