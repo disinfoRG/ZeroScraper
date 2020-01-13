28af09d77d8c48c3523aeb0715ebb1016dd41e34
Pomin Wu <pomin5@gmail.com>
2020-01-13 11:03:50 +0800

    feat: dedup in discover spiders

--- newsSpiders/crawler/discover.py
+++ newsSpiders/crawler/discover.py
@@ -1,4 +1,5 @@
 import json
+import zlib
 from scrapy.crawler import Crawler
 from scrapy.utils.project import get_project_settings
 from newsSpiders.types import SiteConfig
@@ -21,11 +22,22 @@ def create(runner, queries, site_id, args=None):
         "USER_AGENT": site_conf["ua"],
     }
 
+    def links_filter(links):
+        return [
+            l
+            for l in links
+            if queries.get_article_by_url(
+                url=l.url, url_hash=zlib.crc32(l.url.encode())
+            )
+            is None
+        ]
+
     runner.crawl(
         Crawler(DiscoverNewArticlesSpider, settings),
         site_id=site_id,
         site_url=site_url,
         site_type=site_type,
+        links_filter=links_filter,
         article_url_patterns=site_conf["article"],
         following_url_patterns=site_conf["following"],
         selenium="True" if site_conf["selenium"] else "False",
