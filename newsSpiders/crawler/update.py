922f9dd801a01bdceb70b44def4cc0552b06d5f3
Pomin Wu <pomin5@gmail.com>
2020-01-13 10:11:50 +0800

    fix: update crawler bugs

--- newsSpiders/crawler/update.py
+++ newsSpiders/crawler/update.py
@@ -8,13 +8,8 @@ def run(args=None):
     site_conf = SiteConfig.default()
     if args is not None:
         site_conf.update(args)
-    os.system(
-        f"scrapy crawl update_contents \
-                -s DOWNLOAD_DELAY={site_conf['delay']} \
-                -s USER_AGENT='{site_conf['ua']}'"
-    )
     conf = {
-        **get_project_settings,
+        **get_project_settings(),
         "DOWNLOAD_DELAY": site_conf["delay"],
         "USER_AGENT": site_conf["ua"],
     }
