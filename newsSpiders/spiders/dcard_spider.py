4730d2326b205b29f794c8925eb0b6adc52ae5a7
andrea-w-wang <ayw255@nyu.edu>
2020-01-17 23:09:04 +0800

    dcard discover finish

--- newsSpiders/spiders/dcard_spider.py
+++ newsSpiders/spiders/dcard_spider.py
@@ -1,18 +1,18 @@
 import scrapy
 import re
 import json
+import zlib
 from newsSpiders.items import ArticleItem, ArticleSnapshotItem
-import sqlalchemy as db
-from newsSpiders.helpers import generate_next_fetch_time, connect_to_db
+from newsSpiders.helpers import generate_next_fetch_time
 import time
 
 
 class DcardSpider(scrapy.Spider):
-    name = "dcard"
+    name = "dcard_discover"
 
     def __init__(
         self,
-        site_id="",
+        site_id="493",
         site_url="https://www.dcard.tw/f/trending?latest=true",
         site_type="",
         *args,
@@ -32,18 +32,42 @@ class DcardSpider(scrapy.Spider):
         yield scrapy.Request(url=api_url, callback=self.get_post_id)
 
     def get_post_id(self, response):
-        response_json = json.loads(response.body)
+        response_json = json.loads(response.body.decode("utf-8"))
         post_ids = [str(x["id"]) for x in response_json]
 
-        # todo: filter??
-        post_urls = [
-            f"https://www.dcard.tw/f/{self.forum_name}/p/{pid}" for pid in post_ids
-        ]
+        for i in range(len(post_ids)):
+            pid = post_ids[i]
+            comment_api = f"https://www.dcard.tw/_api/posts/{pid}/comments"
+            yield scrapy.Request(
+                url=comment_api,
+                callback=self.get_comments,
+                cb_kwargs={"post_id": pid, "post_info": response_json[i]},
+            )
 
-        comment_api = [
-            f"https://www.dcard.tw/_api/posts/{pid}/comments" for pid in post_ids
-        ]
+    def get_comments(self, response, post_id, post_info):
+        comments_api_result = json.loads(response.body.decode("utf-8"))
 
-    def get_comments(self, response):
+        # prepare Items
+        article = ArticleItem()
+        article_snapshot = ArticleSnapshotItem()
+        # get current time
+        parse_time = int(time.time())
 
-        raise
+        # populate article item
+        article["site_id"] = self.site_id
+        article["url"] = f"https://www.dcard.tw/f/{self.forum_name}/p/{post_id}"
+        article["url_hash"] = zlib.crc32(article["url"].encode())
+        article["article_type"] = "Dcard"
+        article["first_snapshot_at"] = parse_time
+        article["last_snapshot_at"] = parse_time
+        article["snapshot_count"] = 1
+        article["next_snapshot_at"] = generate_next_fetch_time(
+            self.site_type, article["snapshot_count"], parse_time
+        )
+
+        # populate article_snapshot item
+        post_comments = {"post": post_info, "comments": comments_api_result}
+        article_snapshot["raw_data"] = json.dumps(post_comments)
+        article_snapshot["snapshot_at"] = parse_time
+
+        yield {"article": article, "article_snapshot": article_snapshot}
