c0005deced92fed1367f5b54685fd1b9ff196723
Pomin Wu <pomin5@gmail.com>
2020-01-24 21:09:03 +0800

    fix: kill chrome processes

--- run_discover.sh
+++ run_discover.sh
@@ -3,5 +3,4 @@ SLEEP_SEC=${1:-"3000"}
 
 python3 ./batch_discover.py &
 sleep ${SLEEP_SEC}    # 50 minutes
-killall -9 python3
-killall -9 chromedriver
+killall -9 python3 chromedriver chrome google-chrome
