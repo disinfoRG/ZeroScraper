835196d1241862cb9781ce0d60f5538549a62c4a
Pomin Wu <pomin5@gmail.com>
2020-01-25 18:16:36 +0800

    refactor: add main function

--- batch_discover.py
+++ batch_discover.py
@@ -10,19 +10,22 @@ from scrapy.utils.log import configure_logging
 import newsSpiders.runner.discover
 import pugsql
 
-queries = pugsql.module("queries/")
-queries.connect(os.getenv("DB_URL"))
 
-sites = sorted(
-    queries.get_sites_to_crawl(),
-    key=lambda k: 0 if k["last_crawl_at"] is None else k["last_crawl_at"],
-)
+def main():
+    queries = pugsql.module("queries/")
+    queries.connect(os.getenv("DB_URL"))
 
-configure_logging()
-runner = CrawlerRunner(get_project_settings())
-for site in sites:
-    newsSpiders.runner.discover.run(runner, site["site_id"])
-d = runner.join()
-d.addBoth(lambda _: reactor.stop())
+    sites = queries.get_sites_to_crawl()
 
-reactor.run()
+    configure_logging()
+    runner = CrawlerRunner(get_project_settings())
+    for site in sites:
+        newsSpiders.runner.discover.run(runner, site["site_id"])
+    d = runner.join()
+    d.addBoth(lambda _: reactor.stop())
+
+    reactor.run()
+
+
+if __name__ == "__main__":
+    main()
