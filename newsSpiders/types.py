cc46d72b53689249195fb324c24bff2f0317870d
andrea-w-wang <ayw255@nyu.edu>
2020-02-13 17:59:32 -0500

    remove selenium from default types

--- newsSpiders/types.py
+++ newsSpiders/types.py
@@ -26,6 +26,5 @@ class SiteConfig(scrapy.Item):
                 "delay": 1.5,
                 "ua": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
                 "following": "",
-                "selenium": False,
             }
         )
