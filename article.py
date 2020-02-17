3df2c609bac1017b779f0574d61d5e6cb0dff3df
andrea-w-wang <ayw255@nyu.edu>
2020-02-16 22:11:00 -0500

    article.py: add discover with url

--- article.py
+++ article.py
@@ -1,19 +1,21 @@
 from selenium import webdriver
 import requests
-from newsSpiders import helpers
-from newsSpiders.types import SiteConfig
+from url_normalize import url_normalize
 import pugsql
 import os
 import json
 import time
 from random import uniform
 import argparse
+import zlib
+from newsSpiders import helpers
+from newsSpiders.types import SiteConfig
+
 
 queries = pugsql.module("./queries")
 queries.connect(os.getenv("DB_URL"))
 
 
-# feat: discover, update article by url
 def get_article_by_request(url, user_agent):
     headers = {"User-Agent": user_agent}
     r = requests.get(url, headers=headers)
@@ -58,7 +60,9 @@ def update_article_table(article_info, site_info, crawl_time):
     site_type = site_info["type"]
 
     if next_snapshot_at != 0:
-        next_snapshot_at = helpers.generate_next_fetch_time(site_type, 1, crawl_time)
+        next_snapshot_at = helpers.generate_next_fetch_time(
+            site_type, fetch_count=1, snapshot_time=crawl_time
+        )
     queries.update_article_snapshot_time(
         article_id=article_id,
         crawl_time=crawl_time,
@@ -67,9 +71,10 @@ def update_article_table(article_info, site_info, crawl_time):
     )
 
 
-def update(article_id):
+def update(args):
+    print(args.selenium)
     # read url from article_id
-    article_info = queries.get_article_by_id(article_id=article_id)
+    article_info = queries.get_article_by_id(article_id=args.id)
     print(article_info)
 
     # crawler config (ua, selenium) from site
@@ -77,9 +82,10 @@ def update(article_id):
     site_config = json.loads(site_info["config"])
     crawler_config = SiteConfig.default()
     crawler_config.update(site_config)
+    # todo: command line argument should take precedence than config
     user_agent = crawler_config["ua"]
     use_selenium = crawler_config.get("selenium", args.selenium)
-
+    print(use_selenium)
     # get html
     crawl_time = int(time.time())
     if "dcard" in article_info["url"]:
@@ -98,21 +104,84 @@ def update(article_id):
     )
 
 
+def discover(args):
+    # check if args.url exists in db, if so, print message and exist.
+    url = url_normalize(args.url)
+    exist = queries.check_article_existence(url=url)["exist"]
+    if exist == 1:
+        article_id = queries.get_article_by_url(url=url)["article_id"]
+        print(f"The given url exists in the database, having article_id {article_id}.")
+        print(f"Please do update instead.")
+        return article_id
+
+    crawler_config = SiteConfig.default()
+    user_agent = crawler_config["ua"]
+    crawl_time = int(time.time())
+    if "dcard" in args.url:
+        snapshot = get_dcard_article(args.url, user_agent)
+    elif args.selenium:
+        snapshot = get_article_by_selenium(args.url, user_agent)
+    else:
+        snapshot = get_article_by_request(args.url, user_agent)
+
+    article_type = helpers.get_article_type(args.url)
+    url_hash = zlib.crc32(args.url.encode())
+    site_type = helpers.get_site_type(queries, args.site_id)
+    inserted_article_id = queries.insert_article(
+        site_id=args.site_id,
+        url=args.url,
+        url_hash=url_hash,
+        first_snapshot_at=crawl_time,
+        last_snapshot_at=crawl_time,
+        next_snapshot_at=helpers.generate_next_fetch_time(
+            site_type, fetch_count=1, snapshot_time=crawl_time
+        ),
+        snapshot_count=1,
+        redirect_to=None,
+        article_type=article_type,
+    )
+
+    queries.insert_snapshot(
+        article_id=inserted_article_id, crawl_time=crawl_time, raw_data=snapshot
+    )
+    print(f"Finish discover, new article_id = {inserted_article_id}")
+    return inserted_article_id
+
+
 def main(args):
     if args.command == "update":
-        update(args.id)
+        update(args)
+    elif args.command == "discover":
+        discover(args)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     cmds = parser.add_subparsers(title="sub command", dest="command", required=True)
 
+    discover_cmd = cmds.add_parser("discover", help="do discover")
+    discover_cmd.add_argument(
+        "url",
+        type=str,
+        help="url of the article to be snapshot first-time in the db.",
+        nargs="?",
+    )
+    discover_cmd.add_argument(
+        "--selenium", help="use selenium to load website", action="store_true"
+    )
+    discover_cmd.add_argument(
+        "--site_id",
+        help="the site id that this article belongs to. optional.",
+        type=int,
+        default=0,
+    )
+
     update_cmd = cmds.add_parser("update", help="do update")
     update_cmd.add_argument(
-        "id", type=int, help="id of the site to update in news db", nargs="?"
+        "id", type=int, help="id of the article to update in news db", nargs="?"
     )
     update_cmd.add_argument(
-        "--selenium", help="use selenium to update", action="store_true"
+        "--selenium", help="use selenium to load website", action="store_true"
     )
 
     args = parser.parse_args()
