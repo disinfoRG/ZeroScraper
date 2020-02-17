4e0486d71a883f6b972411f99bde3f41d55e67ca
Pomin Wu <pomin5@gmail.com>
2020-02-17 16:00:43 +0800

    feat: use full-text search in monitor api

--- queries/get_publication_by_like.sql
+++ queries/get_publication_by_like.sql
@@ -1,4 +1,4 @@
 -- :name get_publication_by_like :many
 SELECT *
 FROM publication
-WHERE title like :query OR publication_text like :query
+WHERE MATCH(title, publication_text) AGAINST (:query IN BOOLEAN MODE)
