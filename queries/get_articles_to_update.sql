d3b4790e02260f6209aebd92773ca18b62503e2f
Pomin Wu <pomin5@gmail.com>
2020-02-21 11:21:55 +0800

    refactor: remove db queries from update contents spider

--- queries/get_articles_to_update.sql
+++ queries/get_articles_to_update.sql
@@ -7,4 +7,5 @@ WHERE
   AND next_snapshot_at < :current_time
   AND article_type IN ("Article", "PTT")
 ORDER BY next_snapshot_at ASC
+-- set a hard limit on this: 2000 articles per site seems a good chunk of work
 LIMIT 2000
