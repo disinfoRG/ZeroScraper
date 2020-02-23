c1b8b132233437dd9ccb85437fe1df6490f229c2
Pomin Wu <pomin5@gmail.com>
2020-02-23 14:40:45 +0800

    feat: add --limit-sec to execute_spiders

--- execute_spiders.py
+++ execute_spiders.py
@@ -2,9 +2,11 @@ import os
 import argparse
 import time
 import pugsql
-from scrapy.crawler import CrawlerProcess
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
 from newsSpiders.runner import discover, update
+from newsSpiders.process import pid_lock, Cleanup
 
 queries = pugsql.module("queries/")
 queries.connect(os.getenv("DB_URL"))
@@ -26,24 +28,30 @@ parser.add_argument(
 parser.add_argument(
     "-u", "--update", action="store_true", help="execute spider to update articles"
 )
+parser.add_argument("--limit-sec", type=int, help="time limit to run in seconds")
 
 # set up
-args = parser.parse_args()
 
-# execute
-process = CrawlerProcess(get_project_settings())
-if args.discover:
-    crawl_time = int(time.time())
-    queries.update_site_crawl_time(site_id=args.site_id, crawl_time=crawl_time)
-    discover.run(process, args.site_id, vars(args))
-elif args.update:
-    if args.site_id:
-        print(f"Update for site {args.site_id}.")
+with pid_lock(queries, "execute_spider"):
+    args = parser.parse_args()
+    runner = CrawlerRunner(get_project_settings())
+    if args.discover:
+        crawl_time = int(time.time())
+        queries.update_site_crawl_time(site_id=args.site_id, crawl_time=crawl_time)
+        discover.run(runner, args.site_id, vars(args))
+    elif args.update:
+        if args.site_id:
+            print(f"Update for site {args.site_id}.")
+        else:
+            print(f"Update all.")
+        update.run(runner, args.site_id, vars(args))
     else:
-        print(f"Update all.")
-    update.run(process, args.site_id, vars(args))
-else:
-    raise Exception(
-        "Please specify action by adding either --discover or --update flag"
-    )
-process.start()
+        raise Exception(
+            "Please specify action by adding either --discover or --update flag"
+        )
+    d = runner.join()
+    d.addBoth(lambda _: reactor.stop())
+    if args.limit_sec is not None:
+        reactor.callLater(args.limit_sec, Cleanup(runner).terminate)
+
+    reactor.run()
