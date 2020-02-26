c7dade0db644cb3c7ac8b953438513bdca8d0ab5
andrea-w-wang <ayw255@nyu.edu>
2020-02-26 14:48:51 -0500

    remove upsert_site query

--- queries/upsert_site.sql
+++
@@ -1,7 +0,0 @@
--- :name upsert_site :insert
-INSERT INTO Site
-  (airtable_id, is_active, name, type, url, config, site_info)
-VALUES
-  (:airtable_id, :is_active, :name, :type, :url, :config, :site_info)
-ON DUPLICATE KEY UPDATE
-  is_active = :is_active, name = :name, type = :type, url = :url, config = :config
