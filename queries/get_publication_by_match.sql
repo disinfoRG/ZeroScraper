eab68778e5dbaf76b6c68360306b1bba60431e1e
andrea-w-wang <ayw255@nyu.edu>
2020-03-04 22:28:30 -0500

    delete unused publication queries

--- queries/get_publication_by_match.sql
+++
@@ -1,7 +0,0 @@
--- :name get_publication_by_match :many
-SELECT *, MATCH (title, publication_text) AGAINST
-    (:query IN NATURAL LANGUAGE MODE) AS score
-FROM publication
-WHERE MATCH (title, publication_text)
-AGAINST (:query
-    IN NATURAL LANGUAGE MODE)
--- queries/get_publication_count_by_like.sql
+++
@@ -1,4 +0,0 @@
--- :name get_publication_count_by_like :one
-SELECT count(*) as publication_count
-FROM publication
-WHERE title like :query OR publication_text like :query
