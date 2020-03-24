84658f4f147f17ae1a39491a17eeec93e8d0e746
andrea-w-wang <ayw255@nyu.edu>
2020-03-24 00:03:13 -0400

    include url in get_multiple_id_by_url.sql

--- queries/get_multiple_id_by_url.sql
+++ queries/get_multiple_id_by_url.sql
@@ -1,7 +1,7 @@
 -- :name get_multiple_id_by_url :many
 SELECT article_id
 FROM Article
-WHERE url_hash = :url_hash
+WHERE url_hash = :url_hash and url=:url
 order by snapshot_count asc
 limit :limit;
 
