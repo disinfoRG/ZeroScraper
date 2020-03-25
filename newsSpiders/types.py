146679e03616d1dc92f3e46cbc689b664a81b710
andrea-w-wang <ayw255@nyu.edu>
2020-03-25 15:30:34 -0400

    fix: only update not None key-value pair

--- newsSpiders/types.py
+++ newsSpiders/types.py
@@ -1,11 +1,17 @@
 
-class SiteConfig:
+class SiteConfig(dict):
+    def update(self, d, **kwargs):
+        filtered_d = {k: v for k, v in d.items() if v is not None}
+        super().update(filtered_d, **kwargs)
+
     @staticmethod
     def default():
-        return {
+        return SiteConfig(
+            {
                 "depth": 5,
                 "delay": 1.5,
                 "ua": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
                 "following": "",
-                "selenium": False,
+                "selenium": False
             }
+        )
