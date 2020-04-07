1d8cb65bf3e3751d05378bb1e5925d2fa339e632
andrea-w-wang <ayw255@nyu.edu>
2020-04-07 17:28:32 -0400

    script for recover ptt from pttweb

---
+++ recover_ptt.py
@@ -0,0 +1,31 @@
+import json
+import requests
+import os
+import time
+from random import uniform
+import pugsql
+from tqdm import tqdm
+
+queries = pugsql.module("./queries")
+queries.connect(os.getenv("DB_URL"))
+lost_ptt = json.load(open("lost_ptt.json"))
+failed_ptt = list()
+for ptt in lost_ptt:
+    archive_url = ptt["url"].replace("ptt", "pttweb")
+    r = requests.get(archive_url, headers={"User-Agent": "Mozilla 5.0"})
+    if r.status_code == 200:
+        now = int(time.time())
+        queries.insert_snapshot(article_id=ptt["article_id"],
+                                snapshot_at=now,
+                                raw_data=r.text)
+        queries.update_article_snapshot_time(article_id=ptt["article_id"],
+                                             snapshot_count=ptt["snapshot_count"],
+                                             next_snapshot_at=0,
+                                             last_snapshot_at=now)
+        time.sleep(uniform(3, 5))
+        lost_ptt.remove(ptt)
+    else:
+        print("Failed ", ptt)
+        failed_ptt.append(ptt)
+
+json.dump(failed_ptt, open("pttweb_failed.json", "w"))
