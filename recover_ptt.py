ae59204866be6934325e3c31481b3a177d898b13
andrea-w-wang <ayw255@nyu.edu>
2020-04-07 17:30:06 -0400

    do not print insert result

--- recover_ptt.py
+++ recover_ptt.py
@@ -16,17 +16,16 @@ for i in tqdm(range(len(lost_ptt))):
     r = requests.get(archive_url, headers={"User-Agent": "Mozilla 5.0"})
     if r.status_code == 200:
         now = int(time.time())
-        queries.insert_snapshot(article_id=ptt["article_id"],
+        x = queries.insert_snapshot(article_id=ptt["article_id"],
                                 snapshot_at=now,
                                 raw_data=r.text)
-        queries.update_article_snapshot_time(article_id=ptt["article_id"],
+        y = queries.update_article_snapshot_time(article_id=ptt["article_id"],
                                              snapshot_count=ptt["snapshot_count"],
                                              next_snapshot_at=0,
                                              last_snapshot_at=now)
         time.sleep(uniform(3, 5))
         lost_ptt.remove(ptt)
     else:
-        print("Failed ", ptt)
         failed_ptt.append(ptt)
 
 json.dump(failed_ptt, open("pttweb_failed.json", "w"))
