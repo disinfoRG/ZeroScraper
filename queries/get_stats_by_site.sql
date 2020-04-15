1fa5ef53690fb4c0f1ecf4bf1e0e5932bee3b81f
andrea-w-wang <ayw255@nyu.edu>
2020-04-15 23:34:56 +0800

    fix: stats query reformat date as string

--- queries/get_stats_by_site.sql
+++ queries/get_stats_by_site.sql
@@ -1,7 +1,7 @@
 -- :name get_stats_by_site :many
-SELECT *
+SELECT site_id, DATE_FORMAT(date, "%Y-%m-%d") as date, new_article_count, updated_article_count
 FROM SiteStats
 WHERE
   site_id = :site_id
--- 30 days of stats seems quite enough for monitoring purposes
-AND DATE(date) >= DATE_SUB((select max(DATE(date)) from SiteStats) , INTERVAL 30 DAY);
+-- 30 days of stats should be quite enough for monitoring purposes
+AND date >= DATE_SUB((select max(date) from SiteStats) , INTERVAL 30 DAY);
