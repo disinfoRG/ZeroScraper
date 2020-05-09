1b2e649067bd04aa7241fa465a33f4f1c898b10b
Pomin Wu <pomin5@gmail.com>
2020-05-09 10:32:28 +0800

    fix: use disk buffer in snapshot upload script

--- scripts/upload-snapshots.sh
+++ scripts/upload-snapshots.sh
@@ -6,4 +6,6 @@ SNAPSHOT_TABLE=ArticleSnapshot
 OUTPUT_FILE="${SNAPSHOT_TABLE}$(echo ${YESTERDAY} | sed -e 's/\-//g').jsonl.bz2"
 S3_DESTINATION="${S3_DESTINATION_DIR}/${OUTPUT_FILE}"
 
-python3 ${NS_DUMP} -t ${SNAPSHOT_TABLE} -r 1d:${YESTERDAY} | bzip2 -9 | s3cmd put --acl-public - ${S3_DESTINATION}
+python3 ${NS_DUMP} -t ${SNAPSHOT_TABLE} -r 1d:${YESTERDAY} | bzip2 -9 > ${OUTPUT_FILE}
+s3cmd put --acl-public ${OUTPUT_FILE} ${S3_DESTINATION}
+rm ${OUTPUT_FILE}
