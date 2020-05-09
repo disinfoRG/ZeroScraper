f59c922259de357c78c44b121e752f5dcdfb7024
Pomin Wu <pomin5@gmail.com>
2020-05-09 21:18:14 +0800

    fix: smaller batch size for upload-snapshot script

--- scripts/upload-snapshots.sh
+++ scripts/upload-snapshots.sh
@@ -2,10 +2,11 @@
 DATE=${1:-"yesterday"}
 NS_DUMP=./zs-dump.py
 YESTERDAY=$(date --date=$DATE '+%Y-%m-%d')
+BATCH_SIZE=100
 SNAPSHOT_TABLE=ArticleSnapshot
 OUTPUT_FILE="${SNAPSHOT_TABLE}$(echo ${YESTERDAY} | sed -e 's/\-//g').jsonl.bz2"
 S3_DESTINATION="${S3_DESTINATION_DIR}/${OUTPUT_FILE}"
 
-python3 ${NS_DUMP} -t ${SNAPSHOT_TABLE} -r 1d:${YESTERDAY} | bzip2 -9 > ${OUTPUT_FILE}
+python3 ${NS_DUMP} -t ${SNAPSHOT_TABLE} -r 1d:${YESTERDAY} --batch-size ${BATCH_SIZE} | bzip2 -9 > ${OUTPUT_FILE}
 s3cmd put --acl-public ${OUTPUT_FILE} ${S3_DESTINATION}
 rm ${OUTPUT_FILE}
