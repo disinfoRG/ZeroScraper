f19adf13a4314dee7177460b0830f09d1bea3b1e
Pomin Wu <pomin5@gmail.com>
2021-07-17 15:48:04 +0800

    feat: add connection pools to kombu

--- newsSpiders/kombuqueue.py
+++ newsSpiders/kombuqueue.py
@@ -1,17 +1,19 @@
 import os
-import kombu
+from kombu import Connection
+from kombu.pools import connections
 
 queue_url = f"sqla+{os.getenv('DB_URL')}"
 
 
-def connection():
-    return kombu.Connection(
+def connection(block=True):
+    conn = Connection(
         queue_url,
         transport_options={
             "queue_tablename": "kombu_queue",
             "message_tablename": "kombu_message",
         },
     )
+    return connections[conn].acquire(block)
 
 def snapshotsQueue(conn):
     return conn.SimpleQueue("newsSpiders.snapshots")
